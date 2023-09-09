da7c4e0f4c87bddaddfceaecca4ee03ba588d159
Black Slash Creative <ross@blacksla.sh>
2023-09-09 00:21:18 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<img src="https://blacksla.sh/github/img/blackslash-logo.svg" alt="BlackSlash Creative Logo" width="400" />
+
+# gptouille
+A Next.js / React Native app to generate recipes using AI. 
+
+## Screenshots
+coming soon... 
+
+## Features & Roadmap
+- [x] Add open AI integration
+- [ ] User auth
+- [ ] Save & share recipes
+- [ ] ???
+
+## Hire Me :)
+If you're interested in working together, you can email me here: [ghosty@blacksla.sh](mailto:ghosty@blacksla.sh)
+
+## License
+This code is copyright protected under United States law.
---
+++ pages/index.js
@@ -0,0 +1,86 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import { AiFillMinusCircle } from "react-icons/ai";
+
+export default function Home() {
+  const [ingredientInput, setIngredientInput] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [result, setResult] = useState();
+
+  console.log(`ingredients... ${JSON.stringify(ingredients)}`);
+  console.log(`ingredientInput... ${JSON.stringify(ingredientInput)}`);
+
+  async function generateRecipes(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ingredients: ingredients }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data.result);
+      setIngredientInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  const addIngredient = (e) => {
+    e.preventDefault();
+    setIngredients([...ingredients, ingredientInput]);
+    setIngredientInput("");
+  }
+  
+  const removeIngredient = (item)  => {
+    // console.log(`item... ${JSON.stringify(item.ingredient)}`);
+    const leftovers = ingredients.filter((ingtemp) => item.ingredient !== ingtemp);
+    setIngredients(leftovers);
+  }
+
+  const listIngredients = ingredients.map((ingredient) => <button key={ingredient} className={styles.pillbutton} onClick={() => removeIngredient({ingredient})}>{ingredient} <AiFillMinusCircle /></button>);
+
+  return (
+    <div>
+      <Head>
+        <title>gptouille</title>
+        <link rel="icon" href="/rat.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/rat.png" className={styles.icon} />
+        <h3>gptouille</h3>
+        <p className="subtitle">("gee-pee-too-ee" like ratatouille :)</p>
+        <p className="text-center">Hey gptouille, I have these ingredients to use up...<br />What's something healthy I can make?</p>
+        <form onSubmit={addIngredient}>
+          <input
+            type="text"
+            name="ingredient"
+            placeholder="&quot;ginger&quot;"
+            value={ingredientInput}
+            onChange={(e) => setIngredientInput(e.target.value)}
+          />
+          <input type="submit" value="+" />
+        </form>
+        {ingredients && (
+          <div className={styles.ingredientsList}>
+            {listIngredients}
+          </div>
+        )}
+        <button className={styles.generateButton} onClick={generateRecipes}>Generate Recipes</button>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
